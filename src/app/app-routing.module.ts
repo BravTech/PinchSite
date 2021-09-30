fd676efc46627db2e7af7f2fc9b1fa6d87506ebc
Samia <smiftahi@gmail.com>
2021-09-30 12:58:17 -0400

    Search Page - In Progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { ClientComponent } from './register/client/client.component';
 import { ContractorComponent } from './register/contractor/contractor.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register/client', component: ClientComponent },
   { path: 'register/contractor', component: ContractorComponent },
+  { path: 'search', component: SearchComponent },
   { path: '', redirectTo: '/landing', pathMatch: 'full'}
 ];
 
